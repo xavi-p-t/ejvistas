7e14a157cc20d389a8edd38c7b1a783ea6f4637b
xavi-p-t <xavi.palma06@gmail.com>
2025-09-22 18:18:41 +0200

    ª

--- src/main/java/com/project/Controller1.java
+++ src/main/java/com/project/Controller1.java
@@ -3,8 +3,8 @@ package com.project;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.text.Text;
 
 public class Controller1 {
 
@@ -13,11 +13,13 @@ public class Controller1 {
     @FXML
     private AnchorPane container;
     @FXML
-    private Text textoFinal;
+    private Label textoFinal;
+
 
     
-    
-    
+    public void setTexto(String nombre, String edad){
+        this.textoFinal.setText("Hola "+nombre+", tu edad es "+edad);
+    }
    
     @FXML
     private void animateToView0(ActionEvent event) {
